df4691249698c70d6ae331aa92eaaa8bcfb23a8d
Paulo de Tarso <paulo.oinab@gmail.com>
2023-07-11 16:49:22 -0300

    remove App.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './index.css';
-// import App from './App';
 import reportWebVitals from './reportWebVitals';
 import HomePage from './pages/HomePage';
 import ErrorPage from './pages/ErrorPage';
